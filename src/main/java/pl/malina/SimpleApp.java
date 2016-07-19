8b9972b0387c66f801da3782c31e9881cc7c54bb
Dawid Malinowski <d.malinowski@oberthur.com>
2016-07-19 14:11:39 +0200

    Add simple app

---
+++ src/main/java/pl/malina/SimpleApp.java
@@ -0,0 +1,11 @@
+package pl.malina;
+
+public class SimpleApp {
+
+    public static void main(String[] args) {
+        System.out.println("This is very simple app");
+        Simple simple = new Simple();
+        simple.printValue();
+    }
+
+}
